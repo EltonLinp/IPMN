15be0b4ec33b4571bc65e6514affe42d1b0e7f26
EltonLinp <linnan2026work@163.com>
2025-10-31 02:34:24 +0800

    restart

--- trustfusion/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Core orchestration components.
-"""
-
-from .pipeline import TrustFusionSystem, build_default_system
-
-__all__ = ["TrustFusionSystem", "build_default_system"]
--- trustfusion/data/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Data loading utilities for TrustFusion."""
-
-from .dataset import DeepfakeVideoDataset
-
-__all__ = ["DeepfakeVideoDataset"]
